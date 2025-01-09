286ad477f10ba451fd4c0b6cef70fc04f87b4f15
SubashLama123 <lamasubaah2@gmail.com>
2025-01-09 07:15:29 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+// src/App.js
 import React from 'react';
-import NoteForm from './components/NoteForm';
-import NoteList from './components/NoteList';
+import Counter from './features/counter/Counter';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
-      <h1 className="text-3xl font-bold mb-6">Notes App</h1>
-      <NoteForm />
-      <NoteList />
+    <div className="App">
+      <Counter />
     </div>
   );
 }
